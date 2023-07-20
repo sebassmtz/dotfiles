d6540e3471bbeae7877b3c82157c1ff405c2dd8a
sebastian01973 <sebastian.martinez07@uptc.edu.co>
2023-07-19 20:44:05 -0500

    Update .config/polybar/colors.ini Update .config/polybar/config.ini Update .config/polybar/decor.ini Update .config/polybar/launch.sh Update .config/polybar/modules.ini Update .config/polybar/scripts/bluetooth.sh Update .config/polybar/system.ini

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -30,7 +30,8 @@ launch_bar() {
 
 	# Launch the bar
 	for mon in $(polybar --list-monitors | cut -d":" -f1); do
-		MONITOR=$mon polybar -q main -c "$DIR"/config.ini &
+		MONITOR=$mon polybar -q top -c "$DIR"/config.ini &
+		MONITOR=$mon polybar -q bottom -c "$DIR"/config.ini &
 	done
 }
 
